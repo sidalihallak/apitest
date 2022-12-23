805bc0a6a410492117b8bf1c8e6e1962e32edf78
f00104 <sidali.hallak@externe.bnpparibas.com>
2022-12-23 03:59:54 +0100

    bcrypt to bcryptjs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from "../user/user.service";
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
@@ -9,8 +9,7 @@ export class AuthService {
 
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne({"email":email});
-    console.log(await bcrypt.hash(pass, 10));
-    if (user && bcrypt.compare(user.password, await bcrypt.hash(pass, 10))) {
+    if (user && await bcrypt.compare(pass, user.password)) {
       const { password, ...result } = user;
       return result;
     }
